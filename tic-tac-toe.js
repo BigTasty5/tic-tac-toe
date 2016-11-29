67bd3e60a488b513616e76654c4c50524c436073
BigTasty5 <josecortez995@gmil.com>
2016-11-29 10:50:52 -0800

    more

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,7 +9,6 @@ function topleft(){
   circle.setAttribute("r", 30)
   circle.setAttribute("fill","#33ff77")
   canvas.appendChild(circle)
-
 }
   function topmiddle(){
     var canvas = document.getElementById("game-board")
